685b78b5bbddaf40fa29b0e96c44c973b622c6bc
Moizez <moizezhenrique@gmail.com>
2023-03-29 12:27:26 -0300

    Prisma config

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,13 +1,18 @@
 import { NextApiHandler } from "next";
-import { Users } from "../../../constants/usersData";
+import prisma from "../../../libs/prisma";
 
-const handler: NextApiHandler = (req, res) => {
+const handler: NextApiHandler = async (req, res) => {
   const { id } = req?.query;
-  for (let i in Users) {
-    if (String(Users[i].id) === id) {
-      res.json(Users[i]);
-      return;
-    }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id as string),
+    },
+  });
+
+  if (user) {
+    res.json({ status: true, user });
+    return;
   }
 
   res.status(404).json({ error: "Usuário não encontrado!" });
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,8 +1,39 @@
 import { NextApiHandler } from "next";
-import { Users } from "../../../constants/usersData";
+import prisma from "../../../libs/prisma";
+
+const handlerGet: NextApiHandler = async (req, res) => {
+  const users = await prisma.user.findMany({
+    where: {
+      role: "ADMIN",
+    },
+  });
+
+  res.json({ status: true, users });
+};
+
+const handlerPost: NextApiHandler = async (req, res) => {
+  const { name, email, active, role } = req.body;
+
+  const newUser = await prisma.user.create({
+    data: { name, email, active, role },
+  });
+
+  res.status(201).json({ status: true, user: newUser });
+};
 
 const handler: NextApiHandler = (req, res) => {
-  res.json(Users);
+  switch (req.method) {
+    case "GET":
+      handlerGet(req, res);
+      break;
+
+    case "POST":
+      handlerPost(req, res);
+      break;
+
+    default:
+      break;
+  }
 };
 
 export default handler;
